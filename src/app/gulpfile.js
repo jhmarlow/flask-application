6fac6929d70eeb644e2d464733b954276966bffa
jacob <jakemarlow@hotmail.co.uk>
2021-02-20 21:21:00 +0000

    Adding react front end

---
+++ src/app/gulpfile.js
@@ -0,0 +1,22 @@
+// requirements
+
+var gulp = require('gulp');
+var gulpBrowser = require("gulp-browser");
+var reactify = require('reactify');
+var del = require('del');
+var size = require('gulp-size');
+
+
+// tasks
+
+gulp.task('transform', function () {
+  // add task
+});
+
+gulp.task('del', function () {
+  // add task
+});
+
+gulp.task('default', function() {
+  console.log("hello!");
+});
